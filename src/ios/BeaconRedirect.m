cb13d63b7a76d558a21a22a5e479fcc40268d566
NovaDevSas <novadevsas@gmail.com>
2025-02-18 13:50:51 -0500

    Refactor BeaconPlugin: update iOS and Android configurations, add Bluetooth permissions, and implement beacon monitoring

--- src/ios/BeaconRedirect.m
+++ src/ios/BeaconRedirect.m
@@ -3,15 +3,19 @@
 
 @interface BeaconPlugin : CDVPlugin <CLLocationManagerDelegate>
 @property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) CDVInvokedUrlCommand *callbackCommand;
 @end
 
 @implementation BeaconPlugin
 
 - (void)startMonitoring:(CDVInvokedUrlCommand *)command {
+    self.callbackCommand = command;
     self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
     [self.locationManager requestAlwaysAuthorization];
     CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"8C3889DC-3338-FF37-0E19-28BB83B37217"] identifier:@"all-beacons"];
     [self.locationManager startRangingBeaconsInRegion:region];
+    NSLog(@"Started monitoring beacons");
 }
 
 - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray<CLBeacon *> *)beacons inRegion:(CLBeaconRegion *)region {
@@ -22,6 +26,7 @@
             @"minor": @(beacon.minor.integerValue)
         };
         [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result] callbackId:self.callbackCommand.callbackId];
+        NSLog(@"Ranged beacon: %@", result);
     }
 }
 @end
